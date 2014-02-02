e818e1469603e6fa518c739cdbe439bf08db78fe
JayChetty <jaypaulchetty@gmail.com>
2014-02-02 20:12:25 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rota::Application.config.secret_token = '1e175a2532a761ad1911b83c217351a8f2da0d1242859e7552b6d8eaa0a5594077192dbb38a7b15334134d3de0b4461d0e2b3ca62b9ed8d18032358abb7b3a28'
